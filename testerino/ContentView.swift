9c793ab93673d3c0ec97055b29c34aa58619cd55
Estelle <66007150+meijerestelle@users.noreply.github.com>
2021-03-17 00:25:28 +0100

    Initial Commit

---
+++ testerino/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testerino
+//
+//  Created by Estèlle Meijer on 17/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testerino/testerinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  testerinoApp.swift
+//  testerino
+//
+//  Created by Estèlle Meijer on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct testerinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
